199b8b930e5105f29253bf0749b6248db51ace6d
sh.khodjabaev <khodjabayev95@gmail.com>
2018-05-04 18:12:41 +0500

    Added JsonReaderWriter2

--- app/src/main/java/uz/a1uz/a1uzdirector/Activity/Login_Activity.java
+++ app/src/main/java/uz/a1uz/a1uzdirector/Activity/Login_Activity.java
@@ -1,17 +1,12 @@
 package uz.a1uz.a1uzdirector.Activity;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.Configuration;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.Build;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -26,19 +21,13 @@ import android.widget.ToggleButton;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Locale;
 import java.util.Set;
 
-import uz.a1uz.a1uzdirector.Helpers.ActionBarCustomizer;
-import uz.a1uz.a1uzdirector.Helpers.UrlHepler;
 import uz.a1uz.a1uzdirector.JsoN.GetJson;
 import uz.a1uz.a1uzdirector.JsoN.IGetJsonResult;
-import uz.a1uz.a1uzdirector.JsoN.JsonFileWriterReader;
 import uz.a1uz.a1uzdirector.R;
 import uz.a1uz.a1uzdirector.Helpers.UserInfo;
 import uz.a1uz.a1uzdirector.constants.URL_cons;
@@ -48,7 +37,7 @@ public class Login_Activity extends AppCompatActivity {
     AutoCompleteTextView INN;
     EditText etPass;
     SharedPreferences sPref;
-    ArrayAdapter<String> adapter;
+    ArrayAdapter adapter;
     String first="",second="",third="";
     Context context;
     ToggleButton tgButton;
@@ -58,15 +47,13 @@ public class Login_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         sPref = getPreferences(MODE_PRIVATE);
         UserInfo.setLan(sPref.getString("lang", String.valueOf(UserInfo.EheaderLang.eUz)),getBaseContext());
-        System.out.println("OnCreate1");
-        //homeButton=false; // back button
         super.onCreate(savedInstanceState);
-        System.out.println("OnCreate2");
         setContentView(R.layout.activity_login_);
         etPass=(EditText)findViewById(R.id.password);
         btLogin=(Button) findViewById(R.id.btLogin);
         INN=(AutoCompleteTextView)findViewById(R.id.INN);
         tgButton=(ToggleButton)findViewById(R.id.toggleButton);
+        context=this;
 
         tgButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -89,19 +76,21 @@ public class Login_Activity extends AppCompatActivity {
                 }
             }
         });
-        loadText();
+        switch (UserInfo.getLanE()){
+            case eRu:tgButton.setChecked(false);break;
+            case eUz: tgButton.setChecked(true);break;
+        }
         String[] cats = {first, second, third};
         List<String> catList = Arrays.asList(cats);
-        context=this;
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, catList);
-        switch (UserInfo.getLanE()){
-            case eRu:tgButton.setChecked(false);  break;
-            case eUz: tgButton.setChecked(true);break;
+        Set<String> set = sPref.getStringSet(INN_S_KEY, null);
 
+        if(set!=null){
+            adapter = new ArrayAdapter<>(this, R.layout.simple_spinner_dropdown_item,set.toArray(new String[set.size()]));
+            INN.setAdapter(adapter);
         }
-        INN.setAdapter(adapter);
+
         INN.setImeOptions(EditorInfo.IME_ACTION_NEXT);
-        INN.setText(cats[0]);
+        INN.setText(sPref.getString(LASTINN,""));
         INN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -145,8 +134,8 @@ public class Login_Activity extends AppCompatActivity {
             //Toast.makeText(this, "Password is Empty", Toast.LENGTH_SHORT).show();
             return;
         }
-        String url= URL_cons.LOGINRESULT+"/"+INN.getText().toString()+"/"+etPass.getText().toString()+"/"+ UserInfo.getGUID();
-        Log.e("URL",url);
+
+        String url= String.format("%s/%s/%s/%s", URL_cons.LOGINRESULT, INN.getText().toString(), etPass.getText().toString(), UserInfo.getGUID());
         GetJson.execute(url, new IGetJsonResult() {
             @Override
             public void OnBegin() {
@@ -180,7 +169,7 @@ public class Login_Activity extends AppCompatActivity {
                    if(c.getBoolean("IsOk")&&c.getBoolean("IsAuthorization")){
                        UserInfo.setINN(INN.getText().toString());
                        UserInfo.setWidgetListItems(null);
-                       saveText();
+                       mSaveINN(INN.getText().toString());
                        OpenAcitive();
                    }else {
                        Toast.makeText(Login_Activity.this, ""+c.getString("Message"), Toast.LENGTH_SHORT).show();
@@ -200,34 +189,25 @@ public class Login_Activity extends AppCompatActivity {
         Intent intent= new Intent(this, Main_Activity.class);
         startActivity(intent);
     }
-
-    void saveText() {
-        sPref = getPreferences(MODE_PRIVATE);
-        SharedPreferences.Editor ed = sPref.edit();
-        String tmp=INN.getText().toString();
-       if(tmp!=first||tmp!=second||tmp!=third) {
-        ed.putString("first", tmp);
-        ed.putString("second", first);
-        ed.putString("third", second);
-        ed.apply();
-        loadText();
-        adapter.notifyDataSetChanged();}
-
-    }
-
-    void loadText() {
-        sPref = getPreferences(MODE_PRIVATE);
-        first=sPref.getString("first", "");
-        second=sPref.getString("second", "");
-        third=sPref.getString("third", "");
-
-
-    }
     private void SaveLang(String lan){
         sPref = getPreferences(MODE_PRIVATE);
         SharedPreferences.Editor ed = sPref.edit();
         ed.putString("lang", lan);
         ed.apply();
 
+    }
+    public static final String INN_S_KEY="INN";
+    public static final String LASTINN ="INN_last";
+
+    void mSaveINN(String inn){
+        Set<String> set = sPref.getStringSet(INN_S_KEY, null);
+        if(set==null)
+        {set=new HashSet<String>();
+        }
+        set.add(inn);
+        SharedPreferences.Editor editor=sPref.edit();
+        editor.putStringSet(INN_S_KEY, set);
+        editor.putString(LASTINN,inn);
+        editor.apply();
     }
 }
